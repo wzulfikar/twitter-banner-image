dc7ffb11be44187665114dfd1b5b109b4cd307cc
wzulfikar <hey@wzulfikar.com>
2021-04-29 07:52:48 +0700

    Add log. Show helsinki time first.

--- api/update-banner.js
+++ api/update-banner.js
@@ -18,7 +18,7 @@ function getImageUrl(tz1, tz2) {
   const wibTime = getTime("Asia/Jakarta");
   const helsinkiTime = getTime("Europe/Helsinki");
 
-  return `https://og-image.wzulfikar.com/i/**Time%20is**%3Cbr%2F%3E%F0%9F%87%AE%F0%9F%87%A9%20${wibTime}%3Cbr%2F%3E%F0%9F%87%AB%F0%9F%87%AE%20${helsinkiTime}.png?theme=dark&md=1&fontSize=100px&images=NO_IMAGE`;
+  return `https://og-image.wzulfikar.com/i/**Time%20is**%3Cbr%2F%3E%F0%9F%87%AB%F0%9F%87%AE%20${helsinkiTime}%3Cbr%2F%3E%F0%9F%87%AE%F0%9F%87%A9%20${wibTime}.png?theme=dark&md=1&fontSize=100px&images=NO_IMAGE`;
 }
 
 // Fetch image as buffer and convert to base64
@@ -30,10 +30,12 @@ async function getImageBase64(imageUrl) {
 
 export default async function handler(req, res) {
   try {
-    const base64Banner = await getImageBase64(getImageUrl());
+    const imageUrl = getImageUrl();
+    const base64Banner = await getImageBase64(imageUrl);
     await twitterClient.accountsAndUsers.accountUpdateProfileBanner({
       banner: base64Banner,
     });
+    console.log("[INFO] banner updated:", imageUrl);
   } catch (e) {
     console.error("[ERROR] can't update twitter profile:", e);
   }

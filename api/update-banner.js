d1a14df076656573371c98107e721a74b9c36fc8
wzulfikar <hey@wzulfikar.com>
2021-05-01 19:31:25 +0700

    Implement preview param

--- api/update-banner.js
+++ api/update-banner.js
@@ -2,17 +2,34 @@ import country from "./_lib/country";
 import twitter from "./_lib/twitter";
 import { getImageUrl, getImageBase64 } from "./_lib/utils";
 
+function sendImage(res, buffer) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", `image/png`);
+  res.setHeader(
+    "Cache-Control",
+    `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+  );
+  res.end(buffer);
+}
+
 export default async function handler(req, res) {
   try {
     const imageUrl = await getImageUrl(country.finland, country.indonesia);
     const base64Banner = await getImageBase64(imageUrl);
-    await twitter.accountsAndUsers.accountUpdateProfileBanner({
-      banner: base64Banner,
-    });
-    console.log("[INFO] banner updated:", imageUrl);
-    res.send({ ok: true });
+
+    const { preview } = req.query;
+    if (preview === "true") {
+      sendImage(res, Buffer.from(base64Banner, "base64"));
+    } else {
+      await twitter.accountsAndUsers.accountUpdateProfileBanner({
+        banner: base64Banner,
+      });
+      console.log("[INFO] banner updated:", imageUrl);
+      res.send({ ok: true });
+    }
   } catch (e) {
     console.error("[ERROR] can't update twitter profile:", e);
+    res.statusCode = 500;
     res.send({ ok: false, msg: "check logs for error message" });
   }
 }

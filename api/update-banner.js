bd711e82909bcd4ef859ba368cdaf8c085094471
wzulfikar <hey@wzulfikar.com>
2021-05-01 19:40:53 +0700

    Add secret token to prevent unauthorized request to trigger twitter api call

--- api/update-banner.js
+++ api/update-banner.js
@@ -2,6 +2,8 @@ import country from "./_lib/country";
 import twitter from "./_lib/twitter";
 import { getImageUrl, getImageBase64 } from "./_lib/utils";
 
+const SECRET_TOKEN = process.env.SECRET_TOKEN;
+
 function sendImage(res, buffer) {
   res.statusCode = 200;
   res.setHeader("Content-Type", `image/png`);
@@ -17,19 +19,25 @@ export default async function handler(req, res) {
     const imageUrl = await getImageUrl(country.finland, country.indonesia);
     const base64Banner = await getImageBase64(imageUrl);
 
-    const { preview } = req.query;
+    const { preview, token } = req.query;
     if (preview === "true") {
       sendImage(res, Buffer.from(base64Banner, "base64"));
-    } else {
-      await twitter.accountsAndUsers.accountUpdateProfileBanner({
-        banner: base64Banner,
-      });
-      console.log("[INFO] banner updated:", imageUrl);
-      res.send({ ok: true });
+      return;
     }
+
+    // Prevent unauthorized request to trigger twitter update
+    if (token !== SECRET_TOKEN) {
+      res.status(400).send({ ok: false, msg: "bad request" });
+      return;
+    }
+
+    await twitter.accountsAndUsers.accountUpdateProfileBanner({
+      banner: base64Banner,
+    });
+    console.log("[INFO] banner updated:", imageUrl);
+    res.send({ ok: true, msg: "banner updated", image: imageUrl });
   } catch (e) {
     console.error("[ERROR] can't update twitter profile:", e);
-    res.statusCode = 500;
-    res.send({ ok: false, msg: "check logs for error message" });
+    res.status(500).send({ ok: false, msg: "check logs for error message" });
   }
 }
